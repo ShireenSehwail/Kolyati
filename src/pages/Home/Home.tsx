9a5189870c1d28c177695711f793cc143dbdc997
majd khasib <majd.khasib98@gmail.com>
2020-12-12 22:35:05 +0200

    stopped at  an error-going to fix caseCreation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -24,28 +24,13 @@ const Home: React.FC = () => {
  <h2> أحدث الحالات </h2>
 </IonListHeader>
       <IonList dir="rtl" >
-    {/* {casses?.map(data=>(<IonItem><Case 
+        
+    {casses?.map(data=>(<IonItem><Case 
 author={data.author}
 createdTime={data.createdTime}
 title={data.title}
 description={data.description}/></IonItem>
-    ))} */}
-    
-        <IonItem><Case 
-        author={casses![0].author}
-        createdTime={casses![0].createdTime}
-        title={casses![0].title}
-        description={casses![0].description}/></IonItem>
-          <IonItem><Case 
-        author={casses![1].author}
-        createdTime={casses![1].createdTime}
-        title={casses![1].title}
-        description={casses![1].description}/></IonItem>
-          <IonItem><Case 
-        author={casses![2].author}
-        createdTime={casses![2].createdTime}
-        title={casses![2].title}
-        description={casses![2].description}/></IonItem>
+    ))}
 
        </IonList>
      
