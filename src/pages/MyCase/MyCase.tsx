cf1b0c2d40e47dd053d5b29a1dcc4c46fdf0eb97
majd khasib <majd.khasib98@gmail.com>
2020-12-26 19:27:44 +0200

    posted from casecreation it and get it from casedispaly

--- src/pages/MyCase/MyCase.tsx
+++ src/pages/MyCase/MyCase.tsx
@@ -1,26 +1,34 @@
 import { IonHeader, IonToolbar, IonTitle, IonButton, IonMenuButton, IonContent } from '@ionic/react';
 import React,{useState,useEffect} from 'react';
 import Case from '../../components/Case/Case';
-import {LOCAL_STORAGE_KEY_CASE ,LOCAL_STORAGE_KEY_CASE_CREATED} from "../../containers/App";
+import { LOCAL_STORAGE_KEY_CASE_CREATED} from "../../containers/App";
 import PageNotFound from '../PageNotFound/PageNotFound';
+import axios from "axios";
+const api=axios.create({
+  baseURL:`http://localhost:3000/`
+});
 const MyCase: React.FC = () => {
+  
 const [created,setCreated]=useState<string>("");
 
 const [caseState,setCaseState]=useState<{author:string,createdTime:string,title:string,description:string}>();
    //{author:"مجد خصيب",createdTime:"قبل دقيقتين",title:" ساعدوني  !",description:"مرحبًا يا شباب ، أريد أن أسأل عن مساعدتي في العثور على الجامعة الصحيحة التي تتوافق مع احتياجاتي"}
    useEffect(()=>{
-    const caseJSON=localStorage.getItem(LOCAL_STORAGE_KEY_CASE);
     const isCreated=localStorage.getItem(LOCAL_STORAGE_KEY_CASE_CREATED);
-
+    
     if(isCreated!==null)
-    {if(caseJSON!=null)
-    { 
-      const parsedData=JSON.parse(caseJSON);
-      const name=parsedData.name?parsedData.name:"";
-      const description=parsedData.description?parsedData.description:"";
-      setCaseState({author:name,createdTime:"قبل دقيقة واحدة",title:"ساعدوني",description:description})
-      setCreated(isCreated);
-    }
+    {
+      api.get('/').then(res=>{
+       const caseJSON=res.data[0];
+       const name=caseJSON.name?caseJSON.name:"";
+        const description=caseJSON.description?caseJSON.description:"";
+        console.log(res.data);
+
+        setCaseState({author:name,createdTime:"قبل دقيقة واحدة",title:"ساعدوني",description:description})
+        setCreated(isCreated);   
+      })
+      
+    
   }
    
   },[]);
