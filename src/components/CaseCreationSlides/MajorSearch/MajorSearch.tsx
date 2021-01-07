54245ac8ffd3c8a848333b8e760c01aa47ea745f
majd khasib <majd.khasib98@gmail.com>
2021-01-07 23:38:20 +0200

    added flex box

--- src/components/CaseCreationSlides/MajorSearch/MajorSearch.tsx
+++ src/components/CaseCreationSlides/MajorSearch/MajorSearch.tsx
@@ -1,7 +1,10 @@
-import { IonCol, IonGrid, IonItem, IonRow } from "@ionic/react";
+import {  IonGrid} from "@ionic/react";
 import React, { useState } from "react";
 import Major from "./Major/Major";
+
 import Search from "./Search/Search";
+import classes from "./Majors.module.css";
+
  const MajorSearch :React.FC<{majors:{id:any;name:string,acceptanceRate:string,numberOfHours:string,hourRate:string,university:string}[]}>=(props)=>
 { const style={marginRight:"20px" ,color:"#636363"}
 const whiteBackGroundColor={"--ion-background-color":"#fff"};
@@ -14,7 +17,7 @@ return (
 <>
 <h3 style={style}> إختر 3 تخصصات تميل لها</h3>
 <Search onChange={changeSearch}/>
-<IonGrid style={whiteBackGroundColor}>
+<IonGrid style={whiteBackGroundColor} className={classes.Container}>
       {props.majors.map(major=>{
               if(search!==""&&major.name.toLowerCase().indexOf(search!.toLocaleLowerCase())==-1)
             return null;
